a2e0e1931be18299608aaf5888d1362174e5c62a
yangchenglei <931032120@qq.com>
2023-03-30 15:53:34 +0800

    自定义view（1）实现edittext增加去除的x，通过Kotlin写法实现

---
+++ app/src/main/java/com/example/auditiondemo/CustomViewDemo/CustomViewActivity.kt
@@ -0,0 +1,14 @@
+package com.example.auditiondemo.CustomViewDemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.auditiondemo.R
+/*TODO 自定义视图 kotlin版本
+*/
+
+class CustomViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_view)
+    }
+}
---
+++ app/src/main/java/com/example/auditiondemo/CustomViewDemo/EditTextWithClear.kt
@@ -0,0 +1,80 @@
+package com.example.auditiondemo.CustomViewDemo
+
+import android.content.Context
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.widget.EditText
+import androidx.core.content.ContextCompat
+import com.example.auditiondemo.R
+
+class EditTextWithClear @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : androidx.appcompat.widget.AppCompatEditText(context, attrs, defStyleAttr) {
+    //private val iconDrawable=ContextCompat.getDrawable(context,R.drawable.ic_baseline_clear_24)
+    private  var iconDrawable:Drawable?=null
+
+    init {
+        context.theme.obtainStyledAttributes(attrs,
+            R.styleable.EditTextWithClear,
+            0,
+            0
+        ).apply {
+            try {
+                val iconID=getResourceId(R.styleable.EditTextWithClear_clearIcon,0)
+                if (iconID!=0){
+                    iconDrawable= ContextCompat.getDrawable(context,iconID)
+                }
+            }finally {
+                recycle()
+            }
+        }
+    }
+    override fun onTextChanged(
+        text: CharSequence?,
+        start: Int,
+        lengthBefore: Int,
+        lengthAfter: Int
+    ) {
+        super.onTextChanged(text, start, lengthBefore, lengthAfter)
+        toggleClearIcon()
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        event?.let { e->
+            iconDrawable?.let {
+                if(e.action==MotionEvent.ACTION_UP&&
+                    e.x>width-it.intrinsicWidth-20
+                    && e.x<width+20
+                    && e.y>height/2-it.intrinsicHeight/2-20
+                    && e.y<height/2+it.intrinsicHeight/2+20
+                ){
+                    text?.clear()
+                }
+            }
+        }
+        performClick()
+        return super.onTouchEvent(event)
+    }
+
+    override fun performClick(): Boolean {
+        return super.performClick()
+    }
+
+
+    override fun onFocusChanged(focused: Boolean, direction: Int, previouslyFocusedRect: Rect?) {
+        super.onFocusChanged(focused, direction, previouslyFocusedRect)
+        toggleClearIcon()
+    }
+
+    private fun toggleClearIcon(){
+        val icon=if(isFocused && text?.isNotEmpty()==true) iconDrawable else null
+        setCompoundDrawablesRelativeWithIntrinsicBounds(null,null,icon,null)
+    }
+
+
+
+
+
+}
